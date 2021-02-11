e435af96387e0d4cb82a2dc8cef55b1e8e5e222f
Muhammad Naveed <naveed32001@gmail.com>
2021-02-11 16:59:17 +0500

    Initial commit

---
+++ app/src/main/java/com/jumia/ecitizen/constant/AppConstant.java
@@ -0,0 +1,20 @@
+package com.jumia.ecitizen.constant;
+
+public class AppConstant {
+    public static String INTERID="ca-app-pub-9162714698204935/5170992474";
+    public static String BASEURL = "https://www.ecitizen.go.ke/";
+    public static String PRIVACY="Privacy policy\n" +
+            "We built the app as a FREE APP. This SERVICE is provided by us at no cost and is intended for use as is. This page is used to inform visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service. If you choose to use our Service, then you agree to terms of this policy. The Personal Information that we collect is used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy. The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which is accessible at our website unless otherwise defined in this Privacy Policy. Information Collection and Use; We do not collect any information from you however there are third party links that may do, please read there terms of use before pressing on them.\n" +
+            "Log Data\n" +
+            "We want to inform you that whenever you use our Service, in a case of an error in the app we collect data and information\n" +
+            "Cookies\n" +
+            "Cookies are files with a small amount of data that are commonly used as anonymous unique identifiers. These are sent to your browser from the websites that you visit and are stored on your device's internal memory. This Service does not use these “cookies” explicitly. However, the app may use third party code and libraries that use “cookies” to collect information and improve their services. You have the option to either accept or refuse these cookies and know when a cookie is being sent to your device. If you choose to refuse the cookies, you may not be able to use some portions of this Service.\n" +
+            "Links to Other Sites\n" +
+            "This Service contains links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by us. Therefore, we strongly advise you to review the Privacy Policy of these websites. We have no control over and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.\n" +
+            "Children’s Privacy\n" +
+            "These Services do not address anyone under the age of 18. We do not knowingly collect personalized identifiable information from children under 18. In the case we discover that a child under 18 has provided us with personal information, we immediately delete this from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact us so that we will be able to take the necessary action. Changes to This Privacy Policy We may update our Privacy Policy from time to time. Thus, you are advised to review our website page periodically for any changes. These changes are effective immediately they are posted on this page.\n" +
+            "DISCLAIMER:This app may have images that may be obtained from other websites  We don't have any copyright on content of other sites material. This site has its  own terms and policies. We don't use our payment GATEWAY. Payment gateways will be of respective sites itself. We will be not be responsible for any transaction related concern.\n" +
+            "Contact Us\n" +
+            "If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us.\n";
+           ;
+}
